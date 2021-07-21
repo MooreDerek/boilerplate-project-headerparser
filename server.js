b37682c1b8b0a07e0f29111b8a03c2cd68b3203d
Derek Moore <derek.moore@assurity.co.nz>
2021-07-21 21:15:37 +1200

    Start on 0.0.0.0 for IPv4 address

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get("/api/whoami", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, "0.0.0.0", function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
